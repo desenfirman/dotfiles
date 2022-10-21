06c124733a53eba8291d729ba4f336a713b83c66
Dese Narfa Firmansyah <mis.stf08@lnk.co.id>
2022-10-21 22:24:13 +0700

    feat(packages): add apt packages:  golang, jq, socat, golang, wmctrl, xdotool

--- executable_setup_packages_deb.sh
+++ executable_setup_packages_deb.sh
@@ -12,10 +12,18 @@ sudo apt install -y \
 	mysql-client-core-8.0 \
 	libpq-dev \
 	python3-dev \
-	libmysqlclient-dev
+	libmysqlclient-dev \
+	wmctrl \
+	xdotool \
+	jq \
+	socat \
+  golang
+  # wmctrl and xdotool needed for setwindow script
+
 
 sudo snap install tela-icons
 sudo snap install chezmoi --classic
 
+# Install astronomer Airflow astro cli packages
 curl -sSL install.astronomer.io | sudo bash -s
 
