6607b91dc6a49c0206a0c7d93183c338f353cae8
Dese Narfa Firmansyah <desenfirman@pm.me>
2023-09-07 21:36:36 +0700

    fix: change the chezmoi installation to use the official from chezmoi.io

--- install.sh
+++ install.sh
@@ -6,10 +6,11 @@ echo "Downloading OMF"
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install_omf && fish install --noninteractive && rm install_omf
 
 echo "Applying fish config using chezmoi"
-chezmoi init --apply $github_username
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --one-shot $github_username
+
+echo "Install some vim stuff"
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "Change sh to fish"
 chsh -s $(which fish)
-
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
