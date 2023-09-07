7c16bc3b38342e3d24490590fd1cb09e432b9cbf
Dese Narfa Firmansyah <desenfirman@pm.me>
2023-09-07 22:10:11 +0700

    fix: fix omf installation

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 github_username=desenfirman
 
 echo "Downloading OMF"
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install_omf && fish install --noninteractive && rm install_omf
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install_omf && chmod +x install_omf && ./install_omf && rm install_omf
 
 echo "Applying fish config using chezmoi"
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --one-shot $github_username
