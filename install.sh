4cda41d1c374637108ff9ac9812202bf3862ce81
Dese Narfa Firmansyah <desenfirman@pm.me>
2023-09-08 05:43:58 +0700

    fix: set the chezmoi installation to be on top before omf

--- install.sh
+++ install.sh
@@ -2,15 +2,15 @@
 
 github_username=desenfirman
 
-echo "Downloading OMF"
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install && fish install --noninteractive && rm install
-
 echo "Applying fish config using chezmoi"
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --one-shot $github_username
+
+echo "Downloading OMF"
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install && fish install --noninteractive && rm install
 
 echo "Install some vim stuff"
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo "Change sh to fish"
-chsh -s $(which fish)
+# echo "Change sh to fish"
+# chsh -s $(which fish)
